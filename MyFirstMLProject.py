7267243c507d6e81e85263e8da41dc2a9184425e
Yama <41269014+Yama-aa@users.noreply.github.com>
2018-08-07 12:40:44 +0300

    Update MyFirstMLProject.py

--- MyFirstMLProject.py
+++ MyFirstMLProject.py
@@ -2,27 +2,7 @@
 # classification of iris flowers
 # https://machinelearningmastery.com/machine-learning-in-python-step-by-step/
 
-# Check the versions of libraries
-# Python version
-import sys
-print('Python: {}'.format(sys.version))
-# scipy
-import scipy
-print('scipy: {}'.format(scipy.__version__))
-# numpy
-import numpy
-print('numpy: {}'.format(numpy.__version__))
-# matplotlib
-import matplotlib
-print('matplotlib: {}'.format(matplotlib.__version__))
-# pandas
 import pandas
-print('pandas: {}'.format(pandas.__version__))
-# scikit-learn
-import sklearn
-print('sklearn: {}'.format(sklearn.__version__))
-
-# Load libraries
 from pandas.plotting import scatter_matrix
 import matplotlib.pyplot as plt
 from sklearn import model_selection
